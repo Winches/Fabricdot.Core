0bdd5105013e4cc0563d56f24ab5bc0e9db48d05
cyan <green.norton@gmail.com>
2020-10-14 14:33:06 +0800

    add domain exception

---
+++ src/Fabricdot.Domain.Core/SharedKernel/DomainException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Fabricdot.Common.Core.Exceptions;
+
+namespace Fabricdot.Domain.Core.SharedKernel
+{
+    public class DomainException : WarningException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        public DomainException(string message, int code) : base(message, code)
+        {
+        }
+
+        public DomainException(
+            string message,
+            int code,
+            Exception exception) : base(message, code, exception)
+        {
+        }
+    }
+}
