2a1b4daf63b80340cb0d705106b022dabf5c2434
cyan <green.norton@gmail.com>
2020-10-14 14:25:42 +0800

    add domain module

---
+++ src/Fabricdot.Infrastructure.Core/DomainModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Fabricdot.Common.Core.Enumerable;
+using Fabricdot.Infrastructure.Core.DependencyInjection;
+using Fabricdot.Infrastructure.Core.Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fabricdot.Infrastructure.Core
+{
+    public sealed class DomainModule : IModule
+    {
+        private readonly Assembly _assembly;
+
+        public DomainModule(Assembly assembly)
+        {
+            _assembly = assembly;
+        }
+
+        /// <inheritdoc />
+        public void Configure(IServiceCollection services)
+        {
+            DomainProvider.GetServices(_assembly).ForEach(v => services.AddScoped(v.Contract, v.Implementation));
+            DomainProvider.GetRepository(_assembly).ForEach(v => services.AddScoped(v.Contract, v.Implementation));
+        }
+    }
+}
