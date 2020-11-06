64195fb4f7ddf621f901c8578138be910e838224
cyan <green.norton@gmail.com>
2020-11-06 13:45:28 +0800

    add auditing contract

---
+++ src/Fabricdot.Domain.Core/Auditing/IHasDeleterId.cs
@@ -0,0 +1,7 @@
+﻿namespace Fabricdot.Domain.Core.Auditing
+{
+    public interface IHasDeleterId : ISoftDelete
+    {
+        string DeleterId { get; }
+    }
+}
---
+++ src/Fabricdot.Domain.Core/Auditing/IHasDeletionTime.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Fabricdot.Domain.Core.Auditing
+{
+    public interface IHasDeletionTime : ISoftDelete
+    {
+        DateTime? DeletionTime { get; }
+    }
+}
