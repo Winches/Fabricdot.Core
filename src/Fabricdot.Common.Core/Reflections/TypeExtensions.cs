2e948f03ccecceb579cc06a75d88323f90f18724
cyan <green.norton@gmail.com>
2020-12-07 14:58:45 +0800

    add extensions

---
+++ src/Fabricdot.Common.Core/Reflections/TypeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Fabricdot.Common.Core.Reflections
+{
+    public static class TypeExtensions
+    {
+        public static bool IsAssignableToGenericType(this Type givenType, Type genericType)
+        {
+            var interfaceTypes = givenType.GetInterfaces();
+
+            if (interfaceTypes.Any(it => it.IsGenericType && it.GetGenericTypeDefinition() == genericType))
+                return true;
+
+            if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+                return true;
+
+            var baseType = givenType.BaseType;
+            return baseType != null && IsAssignableToGenericType(baseType, genericType);
+        }
+    }
+}
