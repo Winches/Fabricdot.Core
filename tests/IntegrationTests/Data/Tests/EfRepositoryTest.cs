396eda2e2bab00f37bc28cdbc39781e13ccda20d
cyan <green.norton@gmail.com>
2020-12-14 10:44:53 +0800

    update test of repository

--- tests/IntegrationTests/Data/Tests/EfRepositoryTest.cs
+++ tests/IntegrationTests/Data/Tests/EfRepositoryTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using IntegrationTests.Data.Entities;
 using IntegrationTests.Data.Repositories;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +18,7 @@ namespace IntegrationTests.Data.Tests
         }
 
         [Fact]
-        public async Task TestAdd()
+        public async Task TestAddAsync()
         {
             const string id = "A4C59043-DBEC-4A48-919C-3985809AB6F2";
             var book = new Book(id, "Python");
@@ -29,7 +30,7 @@ namespace IntegrationTests.Data.Tests
         }
 
         [Fact]
-        public async Task TestUpdate()
+        public async Task TestUpdateAsync()
         {
             var book = await _bookRepository.GetByNameAsync("CPP");
             const string name = "CPPV2";
@@ -43,7 +44,7 @@ namespace IntegrationTests.Data.Tests
         }
 
         [Fact]
-        public async Task TestDelete()
+        public async Task TestDeleteAsync()
         {
             var book1 = await _bookRepository.GetByNameAsync("CPP");
             await _bookRepository.DeleteAsync(book1);
@@ -54,12 +55,40 @@ namespace IntegrationTests.Data.Tests
         }
 
         [Fact]
-        public async Task TestGetById()
+        public async Task TestGetByIdAsync()
         {
             var book1 = await _bookRepository.GetByNameAsync("CPP");
             var book2 = await _bookRepository.GetByIdAsync(book1.Id);
             Assert.NotNull(book2);
             Assert.Equal(book1.Id, book2.Id);
+        }
+
+        [Fact]
+        public async Task TestGetBySpecAsync()
+        {
+            var book1 = await _bookRepository.GetBySpecAsync(new BookSpecification("CPP"));
+            var book2 = await _bookRepository.GetByIdAsync(book1.Id);
+            Assert.NotNull(book2);
+            Assert.Equal(book1.Id, book2.Id);
+        }
+
+        [Fact]
+        public async Task TestListAsync()
+        {
+            var books = await _bookRepository.ListAsync(new BookSpecification("CPP"));
+            Assert.Equal(1, books.Count);
+            Assert.Equal("CPP", books.Single().Name);
+        }
+
+        [Fact]
+        public async Task TestListAllAsync()
+        {
+            var book = await _bookRepository.GetByNameAsync("CPP");
+            await _bookRepository.DeleteAsync(book);
+            await UnitOfWork.CommitChangesAsync();
+            var books = await _bookRepository.ListAllAsync();
+            Assert.True(books.Count > 1);
+            Assert.DoesNotContain(books, v => v.Name == book.Name);
         }
     }
 }
