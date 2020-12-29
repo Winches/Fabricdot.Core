6b74bafcaddfa121e265794112a267201bf67573
cyan <green.norton@gmail.com>
2020-12-29 15:59:09 +0800

    update domain module

--- src/Fabricdot.Infrastructure.Core/DomainModule.cs
+++ src/Fabricdot.Infrastructure.Core/DomainModule.cs
@@ -1,25 +1,19 @@
-﻿using System.Reflection;
-using Fabricdot.Common.Core.Enumerable;
+﻿using Fabricdot.Common.Core.Reflections;
 using Fabricdot.Infrastructure.Core.DependencyInjection;
 using Fabricdot.Infrastructure.Core.Domain;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Fabricdot.Infrastructure.Core
 {
+    //todo:consider move to infrastructure module
     public sealed class DomainModule : IModule
     {
-        private readonly Assembly _assembly;
-
-        public DomainModule(Assembly assembly)
-        {
-            _assembly = assembly;
-        }
-
         /// <inheritdoc />
         public void Configure(IServiceCollection services)
         {
-            DomainProvider.GetServices(_assembly).ForEach(v => services.AddScoped(v.Contract, v.Implementation));
-            DomainProvider.GetRepository(_assembly).ForEach(v => services.AddScoped(v.Contract, v.Implementation));
+            var assemblies = new TypeFinder().GetAssemblies().ToArray();
+            services.AddRepositories(assemblies);
+            services.AddDomainServices(assemblies);
         }
     }
 }
