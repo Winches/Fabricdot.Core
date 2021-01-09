79f1d14ed25b6419c5cf78465b0bf487395ee92e
cyan <green.norton@gmail.com>
2021-01-09 14:05:36 +0800

    update currentuser service

--- src/Fabricdot.Infrastructure.Core/Security/CurrentUser.cs
+++ src/Fabricdot.Infrastructure.Core/Security/CurrentUser.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Security.Claims;
 using Fabricdot.Common.Core.Security;
 
@@ -6,17 +7,17 @@ namespace Fabricdot.Infrastructure.Core.Security
 {
     public class CurrentUser : ICurrentUser
     {
-        private static readonly Claim[] EmptyClaimsArray = new Claim[0];
+        private static readonly Claim[] EmptyClaimsArray = Array.Empty<Claim>();
 
         private readonly ICurrentPrincipalAccessor _principalAccessor;
 
         public virtual bool IsAuthenticated => !string.IsNullOrEmpty(Id);
 
         public virtual string Id => _principalAccessor.Principal?.Claims
-            ?.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
-            ?.Value ?? string.Empty;
+            .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)
+            ?.Value;
 
-        public virtual string UserName => FindClaim(ClaimTypes.Name)?.Value ?? string.Empty;
+        public virtual string UserName => FindClaim(ClaimTypes.Name)?.Value;
 
         public virtual string[] Roles => FindClaims(ClaimTypes.Role).Select(c => c.Value).ToArray();
 
