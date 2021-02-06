109a8f027a67c6f51f67816c1fbefdd7ab6df56f
cyan <green.norton@gmail.com>
2021-02-06 16:36:59 +0800

    update page query

--- src/Fabricdot.Infrastructure.Core/Queries/PageQueryBase.cs
+++ src/Fabricdot.Infrastructure.Core/Queries/PageQueryBase.cs
@@ -1,20 +1,31 @@
-﻿namespace Fabricdot.Infrastructure.Core.Queries
+﻿using System.ComponentModel.DataAnnotations;
+using Ardalis.GuardClauses;
+
+namespace Fabricdot.Infrastructure.Core.Queries
 {
     public abstract class PageQueryBase<TResult> : IQuery<TResult>
     {
         /// <summary>
+        ///     page number
         /// </summary>
+        [Required]
+        [Range(1, int.MaxValue)]
         public int Index { get; set; }
 
         /// <summary>
+        ///     page size
         /// </summary>
+        [Required]
+        [Range(1, 1000)]
         public int Size { get; set; }
 
         protected PageQueryBase(int index, int size)
         {
+            Guard.Against.OutOfRange(index, nameof(index), 1, int.MaxValue);
+            Guard.Against.OutOfRange(size, nameof(size), 1, 1000);
+
             Index = index;
             Size = size;
-            //todo:querying may construct manually,how to validate it
         }
 
         protected PageQueryBase()
