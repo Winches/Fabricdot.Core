f5fc56610be85707b5c17945fb12bdbc853c2c89
cyan <green.norton@gmail.com>
2021-04-08 17:31:58 +0800

    update action result handler

--- src/Fabricdot.WebApi.Core/Filters/GetActionResultRequestHandler.cs
+++ src/Fabricdot.WebApi.Core/Filters/GetActionResultRequestHandler.cs
@@ -2,15 +2,27 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Fabricdot.WebApi.Core.Endpoint;
+using JetBrains.Annotations;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace Fabricdot.WebApi.Core.Filters
 {
+    [UsedImplicitly]
     public class GetActionResultRequestHandler : IRequestHandler<GetActionResultRequest, IActionResult>
     {
+        private readonly ResultFilterOptions _options;
+
+        public GetActionResultRequestHandler(IOptions<ResultFilterOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        private static Task<IActionResult> GetEmptyResult() => Task.FromResult<IActionResult>(new ObjectResult(NullResponse.Null));
+
         /// <inheritdoc />
-        public Task<IActionResult> Handle(GetActionResultRequest request, CancellationToken cancellationToken)
+        public virtual Task<IActionResult> Handle(GetActionResultRequest request, CancellationToken cancellationToken)
         {
             var context = request.Context;
             var originalResult = context.Result;
@@ -24,7 +36,8 @@ namespace Fabricdot.WebApi.Core.Filters
                         break;
 
                     if (resultValue == null)
-                        return Task.FromResult<IActionResult>(new ObjectResult(NullResponse.Null));
+                        return GetEmptyResult();
+
                     //instance of Response
                     var declaredType = objectResult.DeclaredType;
                     if (declaredType.IsGenericType && declaredType.GetGenericTypeDefinition() == typeof(Response<>))
@@ -34,6 +47,8 @@ namespace Fabricdot.WebApi.Core.Filters
                     return Task.FromResult<IActionResult>(new ObjectResult(ret));
 
                 case EmptyResult _:
+                    if (_options.IncludeEmptyResult)
+                        return GetEmptyResult();
                     break;
 
                 case ContentResult _:
---
+++ src/Fabricdot.WebApi.Core/Filters/ResultFilterOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Fabricdot.WebApi.Core.Filters
+{
+    public class ResultFilterOptions
+    {
+        public bool IncludeEmptyResult { get; set; }
+
+        public ResultFilterOptions()
+        {
+            IncludeEmptyResult = true;
+        }
+    }
+}
