e74a717e1d43c20af9c42d74c34d78af416fad0f
cyan <green.norton@gmail.com>
2021-04-15 10:51:05 +0800

    add concurrency tests

---
+++ tests/Fabricdot.Infrastructure.EntityFrameworkCore.Tests/ConcurrencyStampTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Fabricdot.Infrastructure.EntityFrameworkCore.Tests.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Fabricdot.Infrastructure.EntityFrameworkCore.Tests
+{
+    public class ConcurrencyStampTest : EntityFrameworkCoreTestsBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public ConcurrencyStampTest()
+        {
+            var provider = ServiceScope.ServiceProvider;
+            _bookRepository = provider.GetRequiredService<IBookRepository>();
+        }
+
+        [Fact]
+        public async Task UpdateAsync_WhenEntityChanged_ThrowException()
+        {
+            var book1 = await _bookRepository.GetByNameAsync("CSharp");
+            book1.ChangeName("CSharp1");
+
+            var book2 = await _bookRepository.GetByNameAsync("CSharp");
+            book2.ConcurrencyStamp = Guid.NewGuid().ToString("N");
+
+            await Assert.ThrowsAsync<DbUpdateConcurrencyException>(async () =>
+            {
+                await _bookRepository.UpdateAsync(book1);
+                await UnitOfWork.CommitChangesAsync();
+            });
+        }
+
+        [Fact]
+        public async Task DeleteAsync_WhenEntityChanged_ThrowException()
+        {
+            var book1 = await _bookRepository.GetByNameAsync("CSharp");
+
+            var book2 = await _bookRepository.GetByNameAsync("CSharp");
+            book2.ConcurrencyStamp = Guid.NewGuid().ToString("N");
+
+            await Assert.ThrowsAsync<DbUpdateConcurrencyException>(async () =>
+            {
+                await _bookRepository.DeleteAsync(book1);
+                await UnitOfWork.CommitChangesAsync();
+            });
+        }
+    }
+}
