713e1559b503e31a820984020933b04fc7b947e1
cyan <green.norton@gmail.com>
2021-05-13 14:29:20 +0800

    integrate aspect core

---
+++ src/Fabricdot.Infrastructure.Core/Aspects/AspectCore/AspectCoreInterceptorAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using AspectCore.DynamicProxy;
+using Fabricdot.Core.Aspects;
+using IInterceptor = Fabricdot.Core.Aspects.IInterceptor;
+
+namespace Fabricdot.Infrastructure.Core.Aspects.AspectCore
+{
+    [DisableAspect]
+    public sealed class AspectCoreInterceptorAdapter<TInterceptor> : AbstractInterceptor
+        where TInterceptor : IInterceptor
+    {
+        private readonly TInterceptor _interceptor;
+
+        public AspectCoreInterceptorAdapter(TInterceptor interceptor)
+        {
+            _interceptor = interceptor;
+        }
+
+        /// <inheritdoc />
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            var invocationContext = new AspectCoreInvocationContextAdapter(context, next);
+            await _interceptor.InvokeAsync(invocationContext);
+        }
+    }
+}
---
+++ src/Fabricdot.Infrastructure.Core/Aspects/IInterceptorOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fabricdot.Infrastructure.Core.Aspects
+{
+    public interface IInterceptorOptions
+    {
+        InterceptorCollection Interceptors { get; }
+        List<Type> ExcludeTargets { get; }
+    }
+}
