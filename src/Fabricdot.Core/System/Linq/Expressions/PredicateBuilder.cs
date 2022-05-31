6f7910971ee9274a21e195c705b66a23b06f65b9
green <green.norton@gmail.com>
2022-05-31 14:12:14 +0800

    add predicate builder

---
+++ src/Fabricdot.Core/System/Linq/Expressions/PredicateBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Ardalis.GuardClauses;
+
+namespace System.Linq.Expressions;
+
+// From http://www.albahari.com/nutshell/predicatebuilder.aspx
+public static class PredicateBuilder
+{
+    public static Expression<Func<T, bool>> True<T>() => _ => true;
+
+    public static Expression<Func<T, bool>> False<T>() => _ => false;
+
+    public static Expression<Func<T, bool>> Or<T>(
+        this Expression<Func<T, bool>> expr1,
+        Expression<Func<T, bool>> expr2)
+    {
+        Guard.Against.Null(expr1, nameof(expr1));
+        Guard.Against.Null(expr2, nameof(expr2));
+
+        var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+        return Expression.Lambda<Func<T, bool>>(Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters);
+    }
+
+    public static Expression<Func<T, bool>> And<T>(
+        this Expression<Func<T, bool>> expr1,
+        Expression<Func<T, bool>> expr2)
+    {
+        Guard.Against.Null(expr1, nameof(expr1));
+        Guard.Against.Null(expr2, nameof(expr2));
+
+        var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+        return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+    }
+
+    public static Expression<Func<T, bool>> Compose<T>(
+        this IEnumerable<Expression<Func<T, bool>>> expressions,
+        Func<Expression<Func<T, bool>>, Expression<Func<T, bool>>, Expression<Func<T, bool>>> @operator)
+    {
+        Guard.Against.NullOrEmpty(expressions, nameof(expressions));
+
+        return expressions.Aggregate(@operator);
+    }
+
+    public static Expression<Func<T, bool>> ComposeOr<T>(this IEnumerable<Expression<Func<T, bool>>> expressions)
+    {
+        return expressions.Compose(Or);
+    }
+
+    public static Expression<Func<T, bool>> ComposeAnd<T>(this IEnumerable<Expression<Func<T, bool>>> expressions)
+    {
+        return expressions.Compose(And);
+    }
+}
---
+++ tests/Fabricdot.Core.Tests/System/Linq/Expressions/PredicateBuilderTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using FluentAssertions;
+using Xunit;
+
+namespace Fabricdot.Core.Tests.System.Linq.Expressions;
+
+public class PredicateBuilderTests
+{
+    [Fact]
+    public void True_Should_BeTrue()
+    {
+        PredicateBuilder.True<object>()
+                        .Compile()
+                        .Invoke(null)
+                        .Should()
+                        .BeTrue();
+    }
+
+    [Fact]
+    public void False_Should_BeFalse()
+    {
+        PredicateBuilder.False<object>()
+                        .Compile()
+                        .Invoke(null)
+                        .Should()
+                        .BeFalse();
+    }
+
+    [Fact]
+    public void Or_GivenExpressions_Correctly()
+    {
+        Expression<Func<int, bool>> expr1 = v => v > 5;
+        Expression<Func<int, bool>> expr2 = v => v < 2;
+        var expression = expr1.Or(expr2);
+
+        Enumerable.Range(1, 10)
+                  .Where(expression.Compile())
+                  .ToList()
+                  .Should()
+                  .OnlyContain(v => expr1.Compile().Invoke(v) || expr2.Compile().Invoke(v));
+    }
+
+    [Fact]
+    public void And_GivenExpressions_Correctly()
+    {
+        Expression<Func<int, bool>> expr1 = v => v <= 5;
+        Expression<Func<int, bool>> expr2 = v => v >= 2;
+        var expression = expr1.And(expr2);
+
+        Enumerable.Range(1, 10)
+                  .Where(expression.Compile())
+                  .ToList()
+                  .Should()
+                  .OnlyContain(v => expr1.Compile().Invoke(v) && expr2.Compile().Invoke(v));
+    }
+
+    [Fact]
+    public void ComposeOr_GivenExpressions_Correctly()
+    {
+        Expression<Func<int, bool>> expr1 = v => v > 5;
+        Expression<Func<int, bool>> expr2 = v => v < 2;
+        var expressions = new Expression<Func<int, bool>>[] { expr1, expr2 };
+
+        Enumerable.Range(1, 10)
+                  .Where(expressions.ComposeOr().Compile())
+                  .ToList()
+                  .Should()
+                  .OnlyContain(v => expr1.Compile().Invoke(v) || expr2.Compile().Invoke(v));
+    }
+
+    [Fact]
+    public void ComposeAnd_GivenExpressions_Correctly()
+    {
+        Expression<Func<int, bool>> expr1 = v => v <= 5;
+        Expression<Func<int, bool>> expr2 = v => v >= 2;
+        var expressions = new Expression<Func<int, bool>>[] { expr1, expr2 };
+
+        Enumerable.Range(1, 10)
+                  .Where(expressions.ComposeAnd().Compile())
+                  .ToList()
+                  .Should()
+                  .OnlyContain(v => expr1.Compile().Invoke(v) && expr2.Compile().Invoke(v));
+    }
+}
