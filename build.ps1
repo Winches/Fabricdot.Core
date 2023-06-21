8c8ab9350ee56fe180a83e1e753417fe39215b57
green <green.norton@gmail.com>
2023-06-22 00:31:55 +0800

    fix source of dotnet tool

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 $env:DOTNET_NOLOGO = '1'
 
-dotnet tool restore
+dotnet tool restore --configfile=Nuget.config
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 dotnet cake @args
--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_NOLOGO=1
 
-dotnet tool restore
+dotnet tool restore --configfile=Nuget.config
 
 dotnet cake "$@"
